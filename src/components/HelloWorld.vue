7b6db0c25024c20cbcb3edb43ec1461d1b7bcf6f
原涼馬 <>
2020-07-04 13:27:40 +0900

    初回コミット

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+  <script></script>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Ryoma Hara Portfolio'
+    }
+  },
+  methods: {
+    windowResized(){
+      resizeCanvas(windowWidth, windowHeight);
+      canvasSetup();
+    },
+    setup(){
+      //キャンバスを作成
+      canvas = createCanvas(document.documentElement.scrollWidth,document.documentElement.scrollHeight);
+      canvas.position(0,0);//canvasをページの原点に固定
+      canvas.style('z-index','-1');//canvasを後ろに移動する。
+      //背景色
+      background(255);
+      //図形の線無し
+      noStroke();
+    },
+    draw(){
+      frameRate(30);
+      //オブジェクトの色をランダム（透明度70）
+      fill(random(255),random(255),random(255),70);
+      //キャンバスの中心に直径100pxの丸を描画
+      ellipse(random(width),random(height),random(100));
+    },
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
